8292692db874a0ce7e0fb55f8499b9494f01b2cf
Larry Ewing <lewing@microsoft.com>
2023-11-20 06:14:40 -0600

    Remove dockerfile and docker_build.sh (#30)

--- docker_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -ex
-echo "Building the docker image"
-docker build -t wasi-sdk-builder:latest .
-echo "Building the package in docker image"
-mkdir -p ~/.ccache
-docker run --rm -v "$PWD":/workspace -v ~/.ccache:/root/.ccache -e NINJA_FLAGS=-v --workdir /workspace --tmpfs /tmp:exec wasi-sdk-builder:latest make package LLVM_CMAKE_FLAGS=-DLLVM_CCACHE_BUILD=ON
